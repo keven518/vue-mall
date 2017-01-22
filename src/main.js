5231818892a8a35d530b45a5abe213c1cc61cc5e
kewen <957314724@qq.com>
2017-01-22 22:49:34 +0800

    chapter5

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App';
-import goods from './components/goods/goods.vue';
+import goods from 'components/goods/goods.vue';
+import ratings from 'components/ratings/ratings.vue';
+import seller from 'components/seller/seller.vue';
 
 Vue.use(VueRouter);
 
 /* eslint-disable no-new */
 let routes = [
-  {path: '/', name: 'index', component: App, children: [{path: '/goods', component: goods}]}
+  {path: '/', name: 'index', component: App, children: [{path: '/goods', component: goods}, {path: '/ratings', component: ratings}, {path: '/seller', component: seller}]}
 ];
 let router = new VueRouter({
   'linkActiveClass': 'active',
